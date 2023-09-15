f972ed329398cc032d753242b8f5727d99a0c006
rietvelde <99407273+rietvelde@users.noreply.github.com>
2023-09-15 14:29:55 +0200

    Update az_cli.sh

--- az_cli.sh
+++ az_cli.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-az --version
-
 az login --service-principal -u $APP_ID -p $APP_PASSWORD --tenant $APP_TENANT_ID
 ACI_IP=$(az container show --name $ACI_INSTANCE_NAME --resource-group $ACI_RESOURCE_GROUP --subscription $ACI_SUBSCRIPTION --query ipAddress.ip --output tsv)
 az network private-dns record-set a update --name $DNS_RECORD_NAME --resource-group $DNS_RESOURCE_GROUP --subscription $DNS_SUBSCRIPTION --zone-name $DNS_ZONE_NAME --set aRecords[0].ipv4Address=$ACI_IP
